510da7ff094fb54a8a16f0443f7615c598d1b32f
AdmiraalA <d.vanoosterhoud@gmail.com>
2023-10-23 13:32:54 +0200

    Update backend.tf
    Had a boo-boo

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   required_version = ">= 1.1.0"
   backend "remote" {
-    # organization = "Evilness-ACE"
+    organization = "Evilness-ACE"
     workspaces {
       name = "ace-iac-day-two"
     }
